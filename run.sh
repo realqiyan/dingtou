e393b1c8cbbef7657bd92f26ef6e8d36f880cdc5
qiyan <yhb3420@qq.com>
2021-05-22 03:03:56 +0800

    Revert "调整配置文件位置"
    This reverts commit 6de60439

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup java -jar start/target/start-0.0.1-SNAPSHOT.jar --spring.config.name=../application.yml > application.log  2>&1 &
+nohup java -jar start/target/start-0.0.1-SNAPSHOT.jar --spring.config.name=application.yml > application.log  2>&1 &
