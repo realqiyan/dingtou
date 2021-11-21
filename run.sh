bae9de8efbfe3d2f948b0279a1f7fb0dc20488f1
qiyan <yhb3420@qq.com>
2021-11-21 18:24:47 +0800

    kill

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-# run.sh
-pid=`ps -aux | grep run.sh | awk '{print $2}'`
-echo $pid
-for id in $pid
-do
-kill -9 $id
-echo "kill $id"
-done
 # java
 pid=`ps -aux | grep dingtou | grep java | awk '{print $2}'`
 echo $pid
@@ -16,13 +8,11 @@ kill -9 $id
 echo "kill $id"
 done
 echo "dingtou.jar try starting"
-while true ; do
-  if ! pidof "java" > /dev/null ; then
-    echo "dingtou.jar try running"
-    # fuTuApiIp指定富途服务api服务地址
-    java -jar -DfuTuApiIp=172.18.0.1 -Duser.timezone=Asia/Shanghai dingtou.jar --spring.config.location=./config/application.yml > application.log
-    RET=$?
-    echo "RET CODE: ${RET}"
-  fi
-  sleep 30
-done
+
+if ! pidof "java" > /dev/null ; then
+  echo "dingtou.jar try running"
+  # fuTuApiIp指定富途服务api服务地址
+  java -jar -DfuTuApiIp=172.18.0.1 -Duser.timezone=Asia/Shanghai dingtou.jar --spring.config.location=./config/application.yml > application.log
+  RET=$?
+  echo "dingtou.jar stopped. RET CODE: ${RET}"
+fi
