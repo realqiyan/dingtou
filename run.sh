ad3ed2f98ade01f5da18be92bf380cb6dda9d1f7
qiyan <yhb3420@qq.com>
2021-11-21 19:04:31 +0800

    run

--- run.sh
+++ run.sh
@@ -8,11 +8,13 @@ kill -9 $id
 echo "kill $id"
 done
 echo "dingtou.jar try starting"
-
+while true ; do
 if ! pidof "java" > /dev/null ; then
   echo "dingtou.jar try running"
   # fuTuApiIp指定富途服务api服务地址
-  java -jar -DfuTuApiIp=127.0.0.1 -Duser.timezone=Asia/Shanghai dingtou/start/target/dingtou.jar --spring.config.location=./config/application.yml > application.log
+  java -jar -DfuTuApiIp=127.0.0.1 -Duser.timezone=Asia/Shanghai dingtou/start/target/dingtou.jar --spring.config.location=dingtou/config/application.yml > application.log
   RET=$?
   echo "dingtou.jar stopped. RET CODE: ${RET}"
 fi
+sleep 30
+done
