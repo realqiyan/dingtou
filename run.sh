fe2639dfdd16bc484b25d4cd161a25d6b77a1f78
qiyan <yhb3420@qq.com>
2024-12-17 01:43:28 +0800

    refactor(run.sh): 重构启动脚本
    - 移除了不必要的进程查找和杀死逻辑
    -简化了 Java 进程的启动方式
    - 使用 nohup 命令使进程在后台持续运行
    - 删除了未使用的循环和条件判断结构

--- run.sh
+++ run.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
 # java
-pid=`ps -aux | grep dingtou | grep java | awk '{print $2}'`
-echo $pid
-for id in $pid
-do
-kill -9 $id
-echo "kill $id"
-done
-echo "dingtou.jar try starting"
-#while true ; do
-if ! pidof "java" > /dev/null ; then
-  echo "dingtou.jar try running"
-  # fuTuApiIp指定富途服务api服务地址
-  java -jar -DfuTuApiIp=127.0.0.1 -Duser.timezone=Asia/Shanghai dingtou/start/target/dingtou.jar --spring.config.location=dingtou/config/application.yml > application.log
-  RET=$?
-  echo "dingtou.jar stopped. RET CODE: ${RET}"
-fi
-#sleep 30
-#done
+nohup java -jar -DfuTuApiIp=127.0.0.1 -Duser.timezone=Asia/Shanghai dingtou.jar --spring.config.location=config/application.yml &
